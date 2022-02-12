2651fc74eed1524631ae174751574fd62d6b718f
guiarese <guilhermearesende1@gmail.com>
2022-02-11 22:23:40 -0300

    first commit

---
+++ src/main/java/br/com/guiareze/cacheproject/CacheprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiareze.cacheproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guiareze/cacheproject/CacheprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiareze.cacheproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
